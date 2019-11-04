a76f355a1fa8b9588fa8f917843343744e6e3d9c
thomas dubiel <td1729@hotmail.com>
2019-11-04 22:53:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,8 +68,13 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
+  
 	programmer_hash = 
  		{
+ 		    :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+        },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -83,7 +88,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+  
     
 end
 
